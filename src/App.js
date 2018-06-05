5bd054e65708b5611fbf02ee9cc9fef7977ff6af
TheTeejers <30870766+TheTeejers@users.noreply.github.com>
2018-06-04 19:47:00 -0500

    end of day working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -24,71 +23,78 @@ class App extends Component {
           ]
     }
 
-    // this.getIssues = this.getIssues.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.getIssues = this.getIssues.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // getIssues() {
-  //   axios.get(`https://api.github.com/repos/rails/rails/issues?q=is%3Aopen+is%3Aissue+no%3Aassignee`)
-  //   .then((response) => {
-  //     console.log('I SHOULD HAVE SEARCH RESULTS');
-  //     if (response.data ==='ERROR') {
-  //       alert('Error in fetching data from the Rails Issues API')
-  //     } else {
-  //       console.log(response.data)
-  //       let newIssues = response.data
-  //       for (var i = 0; i < newIssues.length; i++) {
-  //         this.setState({
-  //           info:[
-  //             {
-  //               title: newIssues[i].title,
-  //               milestone: newIssues[i].milestone,
-  //               url: newIssues[i].url,
-  //               html_url: newIssues[i].html_url,
-  //               state: newIssues[i].state,
-  //               user:{
-  //                 login: newIssues[i].user.login,
-  //                 html_url: newIssues[i].user.html_url,
-  //                 avatar_url: newIssues[i].user.avatar_url
-  //               },
-  //               comments: newIssues[i].comments
-  //             }
-  //           ]
-  //         })
-  //       }
-  //       // console.log(this.state)
-  //       console.log(response.data)
-  //     }
-  //   })
-  // }
+  getIssues() {
+    axios.get(`https://api.github.com/repos/rails/rails/issues?q=is%3Aopen+is%3Aissue+no%3Aassignee`)
+    .then((response) => {
+      console.log('I SHOULD HAVE SEARCH RESULTS');
+      if (response.data ==='ERROR') {
+        alert('Error in fetching data from the Rails Issues API')
+      } else {
+        console.log(response.data)
+        let newIssues = response.data
+        for (var i = 0; i < newIssues.length; i++) {
+          this.setState({
+            info:[
+              {
+                title: newIssues[i].title,
+                milestone: newIssues[i].milestone,
+                url: newIssues[i].url,
+                html_url: newIssues[i].html_url,
+                state: newIssues[i].state,
+                user:{
+                  login: newIssues[i].user.login,
+                  html_url: newIssues[i].user.html_url,
+                  avatar_url: newIssues[i].user.avatar_url
+                },
+                comments: newIssues[i].comments
+              }
+            ]
+          })
+                  console.log(newIssues[i].title)
+                  console.log(this.state.info)
+        }
+        console.log(this.state.info)
 
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   this.getIssues();
-  // }
+      }
+    })
+  }
+
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.getIssues();
+  }
 
 
 
   render() {
     console.log(this.state.info.length, 'in render')
-    
+    console.log(this.state.info, 'in render')
+    const child = this.state.info.map((issuesData, i) => {
+            console.log(issuesData.title)
+      return <div key={i}>
+      <p>{issuesData.title}</p>
+
+      <p>{issuesData.state}</p>
+
+    </div>
+    })
 
     return (
 
       <div className="App">
-      {
-        this.state.info.map((issuesData, index) =>
-        <div key={index}>
-          <p>{issuesData.title}</p>
-          <p>{issuesData.state}</p>
 
-        </div>
-        )
-      }
-        // <form onSubmit={this.handleSubmit}>
-        //   <button className='searchButton' onSubmit={this.handleSubmit}>Search Issues</button>
-        // </form>
+        <form onSubmit={this.handleSubmit}>
+          <button className='searchButton' onSubmit={this.handleSubmit}>Search Issues</button>
+        </form>
+              <div>{child}</div>
       </div>
+
     );
   }
 }
